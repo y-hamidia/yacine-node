9baee32ec1b3c27fd0f8019222a9bd9290864b12
y_hamidia <hamidiayacine@gmail.com>
2017-03-12 23:44:08 +0100

    commit to dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! from github')
+  res.send('Hello World! from yacine')
 })
 
 app.listen(process.env.PORT || 3000, function () {
