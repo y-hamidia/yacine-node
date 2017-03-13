474df055b005d7d8535780c0ae5937a556899416
y-hamidia <y-hamidia@users.noreply.github.com>
2017-03-13 08:33:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! from halim')
+  res.send('Hello World! from Hocine')
 })
 
 app.listen(process.env.PORT || 3000, function () {
